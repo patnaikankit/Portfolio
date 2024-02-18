aa8a884068c4deccb9545135ee15a921c72769a4
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-02-18 11:39:05 +0530

    projects

---
+++ src/sections/Experience.tsx
@@ -0,0 +1,100 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+function Experience() {
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    const transformSelected = () => {
+      const underline = document.querySelector<HTMLElement>(".underline");
+      underline!.style.top = `${selected * 2.5}rem`;
+    };
+    transformSelected();
+  }, [selected]);
+
+  const expereinces = [
+    {
+      name: "",
+      role: "",
+      url: "",
+      start: "June 2023",
+      end: "Present",
+      shortDescription: [
+        ""
+      ],
+    },
+    {
+      name: "",
+      role: "",
+      url: "",
+      start: "April 2022",
+      end: "Present",
+      shortDescription: [
+        "",
+      ],
+    },
+  ];
+  return (
+    <motion.div
+      className="experience"
+      id="experience"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+      variants={{
+        visible: { opacity: 1, y: -50 },
+        hidden: { opacity: 0, y: 0 },
+      }}
+    >
+      <div className="title">
+        <h2>Where I&apos;ve Worked</h2>
+      </div>
+      <div className="container">
+        <ul className="exp-slider">
+          <div className="underline"></div>
+          {expereinces.map((expereince, index) => {
+            return (
+              <li
+                className={`exp-slider-item ${
+                  index === selected && "exp-slider-item-selected"
+                }`}
+                onClick={() => setSelected(index)}
+                key={expereince.name}
+              >
+                <span>{expereince.name}</span>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="exp-details">
+          <div className="exp-details-position">
+            <h3>
+              <span>{expereinces[selected].role}</span>
+              <span className="exp-details-position-company">
+                &nbsp;@&nbsp;
+                <Link href={expereinces[selected].url} className="link">
+                  {expereinces[selected].name}
+                </Link>
+              </span>
+            </h3>
+            <p className="exp-details-range">
+              {expereinces[selected].start} - {expereinces[selected].end}
+            </p>
+            <ul className="exp-details-list">
+              {expereinces[selected].shortDescription.map(
+                (description, index) => (
+                  <li key={index} className="exp-details-list-item">
+                    {description}
+                  </li>
+                )
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default Experience;
